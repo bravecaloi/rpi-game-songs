b0fe7715ab2d5abbed1c470f67469095fb987e3d
unknown <guille777@gmail.com>
2015-10-03 15:52:06 -0300

    canciones

--- app/js/Songs.js
+++ app/js/Songs.js
@@ -1,15 +1,15 @@
 SONGS = [
   {
     'position': 0,
-    'name': 'Feliz Cumpleaños',
-    'notes': 'DO DO RE DO FA MI DO DO RE DO SOL FA FA LA DO LA FA FA MI RE SI SI LA FA SOL FA',
-    'times': '01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01'
-  },
-  {
-    'position': 1,
     'name': 'Estrellita',
     'notes': 'DO DO SOL SOL LA LA SOL FA FA MI MI RE RE DO SOL SOL FA FA MI MI RE SOL SOL FA FA MI MI RE DO DO SOL SOL LA LA SOL FA FA MI MI RE RE DO',
     'times': '01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01'
+  },
+  {
+    'position': 1,
+    'name': 'Feliz Cumpleaños',
+    'notes': 'DO DO RE DO FA MI DO DO RE DO SOL FA FA LA DO LA FA FA MI RE SI SI LA FA SOL FA',
+    'times': '01 01 01 01 01 02 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01'
   },
   {
     'position': 2,
@@ -34,7 +34,23 @@ SONGS = [
     'name': 'La Cucaracha',
     'notes': 'DO DO DO FA LA DO DO DO FA LA FA FA MI MI RE RE DO DO DO DO MI SOL DO DO DO MI SOL DO RE DO SI LA SOL FA',
     'times': '01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01'
+  },
+  {
+    'position': 6,
+    'name': 'Noche de paz',
+    'notes': 'SOL LA SOL MI SOL LA SOL MI RE RE SI DO DO SOL LA LA DO SI LA SOL LA SOL MI LA LA DO SI LA SOL LA SOL MI RE RE RE FA RE SI DO MI DO SOL MI SOL FA RE DO RE RE RE FA RE SI DO MI DO SOL MI SOL FA RE DO',
+    'times': '01 01 01 02 01 01 01 02 01 01 01 01 01 02 01 01 01 01 01 01 02 01 01 01 01 01 01 01 01 02 01 01 01 01 01 01 02 01 01 01 01 01 01 02 01 001 01 01 01 01 01 01 02 01 01 01 01 01 01 02'
+  },
+  {
+    'position': 7,
+    'name': 'Fray Jacobo',
+    'notes': 'DO RE MI DO DO RE MI DO MI FA SOL MI FA SOL SOL LA SOL FA MI DO SOL LA SOL FA MI DO DO SOL DO DO SOL DO',
+    'times': '01 01 01 01 01 01 01 01 01 01 002 01 01 002 001 01 001 01 01 01 001 01 001 01 01 01 01 001 02 01 001 01'
   }
+
+
+
+  
 ];
 
 TONES = ['DO','RE','MI','FA','SOL','LA','SI'];
